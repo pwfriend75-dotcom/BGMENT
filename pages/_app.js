b7e057fbd36301a54439966c57f5d597dd545255
pwfriend75-dotcom <pwfriend75@gmail.com>
2026-09-10 13:31:23 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
+import '../styles/globals.css';
+
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import '../styles/globals.css'; // 글로벌 CSS 스타일 (Tailwind CSS)
 
 export default function App({ Component, pageProps }) {
   return (
-    <div className="min-h-screen flex flex-col bg-black text-white font-sans">
+    <>
       <Header />
-      <main className="flex-grow">
-        <Component {...pageProps} />
-      </main>
+
+      <Component {...pageProps} />
+
       <Footer />
-    </div>
+    </>
   );
 }
