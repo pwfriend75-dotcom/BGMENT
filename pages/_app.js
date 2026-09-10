4cf4ef4d29d3c3fcdc943afc274233976d808902
pwfriend75-dotcom <pwfriend75@gmail.com>
2026-09-10 14:05:05 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function App({ Component, pageProps }) {
   return (
